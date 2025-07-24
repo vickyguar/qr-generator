3110fe368358b53098a8be62506c8b5132cbe109
vickyguar <vickyguarnieri1@gmail.com>
2025-07-24 16:19:18 -0300

    Cuestiones pintabotones

--- constants.py
+++ constants.py
@@ -1,7 +1,13 @@
-from reportlab.lib.pagesizes import A6
+from PIL import Image
 
-PAGE_WIDTH, PAGE_HEIGHT = A6
 PLANTILLA_PATH = "Plantilla.png"
+# TODO: Plantilla2 (para el frente)
+
+with Image.open(PLANTILLA_PATH) as img:
+    PAGE_WIDTH, PAGE_HEIGHT = img.size
+    PAGE_WIDTH = PAGE_WIDTH // 2
+    PAGE_HEIGHT = PAGE_HEIGHT // 2
+
 QR_SIZE = 150  
 PDF_OUTPUT_DIR = "pdf_contactos"
 ZIP_OUTPUT_PATH = "contactos_qr_congreso.zip"
